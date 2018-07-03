52d74b94789d5076e4a85c2934b20c4554f40309
Sandy Wang <sandyyw98@gmail.com>
2018-07-03 18:20:57 -0400

    try to accomplish #3

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const inputController = require('./controllers/inputController')
+var inputController = require('./controllers/inputController');
 
 var addRouter = require('./routes/add');
 var welcomeRouter = require('./routes/welcome');
@@ -23,6 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/add', addRouter);
+app.use('/saveinput',inputController.saveInput);
+
 app.use('/', welcomeRouter);
 
 // catch 404 and forward to error handler
