ec8331aeb244dbcb4cc4b952dff352ae1afb7643
zqhuagn <zqhuang@brandeis.edu>
2018-07-03 13:58:16 -0400

    welcome

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var addRouter = require('./routes/add');
+var welcomeRouter = require('./routes/welcome');
+
 
 var app = express();
 
@@ -18,7 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/add', addRouter);
+app.use('/', welcomeRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/add.js
+++ routes/add.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('indexBS', { title: 'Express' });
+  res.render('add', { title: 'SON' });
 });
 
 module.exports = router;
