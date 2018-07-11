a8febd771c6efb1264188d6da8112a618d4095ce
XTLiuuu <liuxuantong0611@gmail.com>
2018-07-10 20:06:07 -0400

    Alexa

--- controllers/helloDFController.js
+++ controllers/helloDFController.js
@@ -1,89 +1,76 @@
 'use strict';
 const Schedule = require( '../models/Schedule' );
 console.log("loading the schedule Controller")
-var data = [];
 
 exports.process_request =  (req, res) => {
   console.dir(req.body)
   console.log("in process_request")
   var output_string;
   var name;
+ // this is how we define the result
+  var result = {
+    "version": "beta",
 
-  if(req.body.request){
-    // this is the same as getting the intent name
-    console.log(req.body.request);
-    if(req.body.request.intent.name == "add_event"){
-      console.log("in Add_Event");
-      output_string = addEvent(req.body.request);
-    }
-    else if(req.body.request.intent.name == "ask_event"){
-      console.log("in Ask_Event");
-      output_string = askEvent(req.body.request);
-    }
-    return res.json({
-      "version": "beta",
-
-      "sessionAttributes":{
-        "key":"value"
+    "sessionAttributes":{
+      "key":"value"
+    },
+    "response": {
+      "outputSpeech": {
+        "type": "PlainText",
+        //"text": output_string
       },
-      "response": {
+      "reprompt": {
         "outputSpeech": {
           "type": "PlainText",
-          "text": output_string
-        },
-        "reprompt": {
-          "outputSpeech": {
-            "type": "PlainText",
-            "text": "Plain text string to speak reprompt"
-          }
-        },
-        "shouldEndSession": true
-      }
-    });
-  }
+          "text": "Plain text string to speak reprompt"
+        }
+      },
+      "shouldEndSession": true
+    }
+  };
 
-  // welcome and initialize list
-  if(req.body.queryResult.intent.name == "projects/son-bjwhqg/agent/intents/0a65ec9a-eddd-47bb-b7d3-bec8204c1c58"){
-    console.log("in Welcome");
-    name = req.body.queryResult.parameters["given-name"]
-    output_string = welcome(name);
-    var count = 0;
-  }
-
-  // add schedule
-  else if(req.body.queryResult.intent.name == "projects/son-bjwhqg/agent/intents/9deb94c8-c91c-4033-aec3-b753a4f59870"){
+  if(req.body.request.intent.name == "add_event"){
     console.log("in Add_Event");
-    output_string = addEvent(req.body.queryResult);
+    output_string = addEvent(req.body.request);
+    result.response.outputSpeech.text = output_string;
+    res.json(result);
   }
 
-  // broadcast all schedules
-  // e.g. What do I need to do before three p.m.?
-  // the current version is ask about a certain time and return one thing
-  // e.g. What am I going to do at 8 pm
-  // you cannot ask for a time interval
-  else if(req.body.queryResult.intent.name == "projects/son-bjwhqg/agent/intents/d4489da3-80c8-4692-ad9d-4530e2cda7e8"){
-    console.log("in Broadcast_Event");
-    var time = req.body.queryResult.parameters["time"]
-    output_string = bcEvent(time);
-  }
+  else if(req.body.request.intent.name == "ask_event"){
+    console.log("in Ask_Event1");
+    var time = req.body.request.intent.slots.time["value"];
+    console.log("time is " + time);
+    var date = req.body.request.intent.slots.date["value"];
+    console.log("date is " + date);
+    Schedule.find({
+      time: time,
+      date: date,
+    }, function(err, schedule_list){
+      if(err){
+        console.log( error.message );
+      } else {
+        if(schedule_list.length == 0){
+          output_string = "You have nothing scheduled for " + date + " at " + time
+        } else {
+          console.log("schedule 2 is " + schedule_list)
+          if(schedule_list.length == 1){
+            output_string =  time + " on " + date + " : "+ schedule_list[0].schedule;
+          }
+          else{
+            output_string = time + " on " + date + " : "+ schedule_list[0].schedule;
+            for(var i = 1; i < schedule_list.length; i ++){
+              output_string = output_string + " , " + schedule_list[i].schedule;
+            }
+          }
+          console.log("output_string = " + output_string)
+        }
+      }
 
-  /**
-  // delete event
-  else if(req.body.queryResult.intent.name == "projects/son-bjwhqg/agent/intents/88f75d90-304f-4311-bffa-507fc91d82ac"){
-    console.log("in Delete_Event");
-    output_string = exports.deleteEvent(req.body.queryResult);
+      result.response.outputSpeech.text = output_string;
+      res.json(result);
+    })
   }
-  */
-  return res.json({
-              "fulfillmentMessages": [],
-              "fulfillmentText": output_string,
-              "payload": {"slack":{"text": output_string}},
-              "outputContexts": [],
-              "source": "Text Source",
-              "followupEventInput": {}
-          });
 };
-
 
 
 function welcome(name) {
@@ -105,64 +92,45 @@ function addEvent(req){
   console.log("duration is " + duration);
   var text = req.intent.slots.event["value"];
   console.log("event is " + text);
+  if(date == null){
+    var today = new Date();
+    date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  }
   // user's input include time and date
-  if(time || date){
-    if(date){
-      response = "Okay, I will remind you on " + date + "at" + time + " ."
-    }
-    else{
-      response = "Okay, I will remind you at " + time + " ."
-    }
+  if(time){
+    response = "Okay, I will remind you on " + date + "at" + time + " ."
   }
   else if(duration){
+    console.log("in duration")
+    console.log("duration1 is " + duration)
     var end = text.indexOf('in')
+    console.log('text')
     text = text.slice(0, end)
     response = "Okay, I will remind you in " + duration
-  }
+    console.log("type is " + duration.slice(-1));
+    var type = duration.slice(-1);
+    var end1 = duration.index('T');
+    var num = text.slice(end1, -1);
+    console.log("num = " + num)
+    /**
+    var d = new Date();
+    d.getHours();
+    d.getMinutes();
+    d.getSeconds();
+    //time =
+    //date =
+    */
+  }/**
   let newSchedule = new Schedule ({
     time: time,
     date: date,
     schedule: text
   })
-  data.push(newSchedule);
-  console.log("data length is " + data.length);
   newSchedule.save()
   console.log("time is " + newSchedule.time);
   console.log("date is " + newSchedule.date);
   console.log("schedule is " + newSchedule.schedule);
-  return response;
-}
-
-function askEvent(req){
-  console.log("in askEvent");
-  var response;
-  var time = req.intent.slots.time["value"];
-  console.log("time is " + time);
-  var date = req.intent.slots.date["value"];
-  console.log("date is " + date);
-  if(time){
-    for(var i = 0; i < data.length; i ++){
-      if(data[i].time == time){
-        response = "You will " + data[i].schedule + " at " + time;
-      }
-    }
-  }
-  else if(date){
-    for(var i = 0; i < data.length; i ++){
-      if(data[i].date == date){
-        console.log(data[i].date)
-        response = "You will " + data[i].schedule + " on " + date;
-      }
-    }
-  }
-  if(response == null){
-    if(time){
-      response = "There is no schedule at " + time
-    }
-    else if(date){
-      response = "There is no schedule on " + date
-    }
-  }
+  */
   return response;
 }
 
