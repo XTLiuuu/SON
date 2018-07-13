5db9a1affc558896e84d09fcff27820a35b015ba
XTLiuuu <liuxuantong0611@gmail.com>
2018-07-13 10:12:15 -0400

    conflict

--- models/Schedule.js
+++ models/Schedule.js
@@ -6,10 +6,7 @@ var scheduleSchema = mongoose.Schema( {
   count: Number,
   time: String,
   date: String,
-<<<<<<< HEAD
-=======
   date1: Date, //used for calculate duration 
->>>>>>> a87abd76825745909e57b53fd72b2ae065b290b3
   schedule: String
 } );
 
