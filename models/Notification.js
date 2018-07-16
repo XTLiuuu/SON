9c66b677709c6cad15f13875439363d9703f824c
Sandy Wang <sandyyw98@gmail.com>
2018-07-16 14:53:26 -0400

    quq

--- models/Notification.js
+++ models/Notification.js
@@ -4,7 +4,8 @@ const mongoose = require( 'mongoose' );
 var notificationSchema = mongoose.Schema( {
   type: String,
   title: String,
-  content: String
+  content: String,
+  email: String
 } );
 
 module.exports = mongoose.model( 'Notification', notificationSchema );
