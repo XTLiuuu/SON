71e83ab8ce5c9db16d4df98bb46b97fe9247a4e6
Sandy Wang <sandyyw98@gmail.com>
2018-07-30 14:25:05 -0400

    Fixed Google Authentication callbackURL

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'googleAuth' : {
         'clientID': '276977230243-kb5oku2tf0h5r8pm7mh9oh6ltdt6nouj.apps.googleusercontent.com',
         'clientSecret'  : 'A9nfx7cgpn0dAM53Ff_30fV9',
-        'callbackURL'   : 'http://127.0.0.1:7700/login/authorized'
+        'callbackURL'   : '/login/authorized'
         //'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:7700/login/authorized'
     }
 
